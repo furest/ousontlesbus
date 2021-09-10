0df8dc166f771bfb8f867a3cada8f1756a623e62
furest <yannick.diepart@hotmail.com>
2021-09-10 22:50:32 +0200

    rework app.py to work with new faster hercules search

--- wsgi_scripts/findplate/app.py
+++ wsgi_scripts/findplate/app.py
@@ -4,7 +4,8 @@ from flask import Flask, abort, jsonify
 import json
 import re
 
-ZONE01_URL = "https://www.zone01.be/zoeken"
+#ZONE01_URL = "https://www.zone01.be/zoeken"
+ZONE01_URL = "https://www.zone01.be/hercules/resultaat_uitgebreid"
 #bus_id = 5736
 
 
@@ -14,33 +15,28 @@ app = Flask(__name__)
 def resource_not_found(e):
     return jsonify(error=str(e)), 404
 
+
+
 @app.route('/find/<bus_id>')
 def bus(bus_id):
-    resp = requests.get(url=ZONE01_URL, params={"s":bus_id})
+    data = {
+        "owner": "OTW",
+        "busnr": bus_id,
+        "status":"A",
+        "submit": "Zoeken"
+    }
+    resp = requests.post(url=ZONE01_URL, data=data)
     soup = BeautifulSoup(resp.content, features="lxml")
-    pagination = soup.find(class_="Zebra_Pagination")
-    if pagination:
-        lastpage = int(pagination.find_all("li")[-2].text)
-        pages = range(1,lastpage+1)
-    else:
-        pages = range(1,2)
-    for i in pages:
-        bus_table = soup.find("table",id="example")
-        if bus_table is None:
-            abort(404, "Bus ID not found")
-        busses = bus_table.find_all(id="herculesdetails")
-        for bus in busses:
-            details = bus.find_all("tr")
-            for detail in details:
-                if not detail.find(class_="owners",string=re.compile("OTW\s\(.+\)")):
-                    continue
-                if not detail.find(class_="busnr", string=str(bus_id)):
-                    continue
-                if not detail.find(alt="status",src=re.compile("/A.png$")):
-                    continue
-                license_plate = detail.find("span", class_="kenteken")
-                href = detail.find("a").get("href")
-                return jsonify(license_plate=license_plate.text, link="https://www.zone01.be"+str(href))
-        nextresp = requests.get(url=ZONE01_URL, params={"s":bus_id, "page":i+1})
-        soup = BeautifulSoup(nextresp.content)
+    bus_table = soup.find(id="herculesdetails")
+    if bus_table is None:
+        abort(404, "Bus ID not found")
+    busses = bus_table.findAll("tr")
+    for bus in busses:
+       bus_owner = bus.find("span", class_="owners")
+       bus_nr = bus.find("span", class_="busnr")
+       license_plate = bus.find("span", class_="kenteken")
+       href = bus.find("a").get("href")
+       if "OTW" in bus_owner.text and bus_nr.text == str(bus_id):
+           return jsonify(license_plate=license_plate.text, link="https://www.zone01.be/hercules/"+str(href))
     abort(404, "Bus ID not found")
+
